c5f1cd5d46241801e4e7b555d0c3659cd4baaaee
James Muscat <james.muscat@alces-software.com>
2017-03-03 15:43:12 +0000

    Some inital repository functions for cluster customizer.

---
+++ clusterware-customize/lib/functions/customize-repository.functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+require ruby
+
+customize_repository_get_url() {
+  local conffile name
+  name="$1"
+  conffile="${2:-${cw_ROOT}/etc/cluster-customizer/config.yml}"
+
+  ruby_run <<\RUBY
+require 'yaml'
+repo_list = YAML.load_file("${conffile}")["repositories"]
+
+if repo_list.key? "${name}"
+  puts repo_list["${name}"]
+end
+RUBY
+}
+
+customize_repository_type() {
+  local url
+  url="$1"
+  if [[ "$url" == "http:/"* || "$url" == "https:/"* ]]; then
+    echo "http"
+  elif [[ "$url" == "s3:/"* ]]; then
+    echo "s3"
+  elif [[ "$url" == "/"* ]]; then
+    echo "file"
+  fi
+}
