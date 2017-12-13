df5470414881517d6a9b1673b419db3e419a0813
Mark J. Titorenko <mark.titorenko@alces-software.com>
2017-12-13 16:18:15 +0000

    File system user directory and home directory link creation helper.

---
+++ clusterware-fshelper/etc/profile.d/80-fshelper.sh
@@ -0,0 +1,9 @@
+################################################################################
+##
+## Alces Clusterware - Shell configuration
+## Copyright (c) 2017 Alces Software Ltd
+##
+################################################################################
+if [ -x "${cw_ROOT}"/libexec/share/setup-fslinks ]; then
+    "${cw_ROOT}"/libexec/share/setup-fslinks
+fi
