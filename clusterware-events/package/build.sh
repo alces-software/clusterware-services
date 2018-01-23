5986c79fe1623e409f43ec97fea526d778e22f46
James Muscat <james.muscat@alces-software.com>
2018-01-23 15:03:55 +0000

    Forge-package-ify `clusterware-events`.

---
+++ clusterware-events/package/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+package_name='clusterware-events'
+
+if [ -f ./${package_name}.zip ]; then
+  echo "Replacing existing ${package_name}.zip in this directory"
+  rm ./${package_name}.zip
+fi
+
+temp_dir=$(mktemp -d /tmp/${package_name}-build-XXXXX)
+
+cp -r * "${temp_dir}"
+rm -f "${temp_dir}"/data/etc/handlers/.gitkeep
+
+pushd "${temp_dir}" > /dev/null
+zip -r ${package_name}.zip *
+popd > /dev/null
+
+mv "${temp_dir}"/${package_name}.zip .
+
+rm -rf "${temp_dir}"
