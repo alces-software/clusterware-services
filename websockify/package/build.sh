bf59b278e04a83e281152f8f9b9727aeffdb33ab
James Muscat <james.muscat@alces-software.com>
2018-01-30 14:39:48 +0000

    Fix websockify packagification.
    That's not a word. I just made that up.

--- websockify/package/build.sh
+++ websockify/package/build.sh
@@ -14,7 +14,7 @@ cp -r * "${temp_dir}"
 
 curl -L "https://github.com/kanaka/websockify/archive/v0.7.0.tar.gz" -o /tmp/websockify-source.tar.gz
 tar -C /tmp -xzf "/tmp/websockify-source.tar.gz"
-pushd /tmp/websockify-*
+pushd /tmp/websockify-0* > /dev/null
 mkdir -p "${temp_dir}/data/opt/websockify/lib"
 cat <<\EOF > "${temp_dir}/data/opt/websockify/websockify"
 #!/bin/bash
@@ -23,7 +23,7 @@ exec ./run "$@"
 EOF
 chmod 755 "${temp_dir}/data/opt/websockify/websockify"
 cp -R websockify run README.md LICENSE.txt docs "${temp_dir}/data/opt/websockify/lib"
-popd
+popd > /dev/null
 
 pushd "${temp_dir}" > /dev/null
 zip -r ${package_name}.zip *
