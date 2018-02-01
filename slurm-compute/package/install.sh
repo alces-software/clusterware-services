3960d9ce7b315ad02d52fc6fb96d62c4d275e4e9
James Muscat <james.muscat@alces-software.com>
2018-02-01 11:30:47 +0000

    Forge package for Slurm compute node configuration.

---
+++ slurm-compute/package/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+require files
+files_load_config distro
+
+if [[ "$cw_DIST" == "el6" ]]; then
+  echo "Slurm is not supported on EL6"
+  exit 1
+fi
+
+sed -e "s,_cw_ROOT_,${cw_ROOT},g" \
+    data/init/systemd/clusterware-slurm-slurmd.service \
+    > /etc/systemd/system/clusterware-slurm-slurmd.service
+cat <<EOF > /etc/tmpfiles.d/clusterware-slurm.conf
+# Clusterware Slurm runtime directory
+d /run/slurm 0755 slurm root -
+EOF
