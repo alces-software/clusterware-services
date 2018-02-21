09feb789a2d1871e16b4122b5041e251bb892209
James Muscat <james.muscat@alces-software.com>
2018-02-21 12:24:46 +0000

    Correct package name.

--- clusterware-hosts/package/build.sh
+++ clusterware-hosts/package/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-package_name='package-skel'
+package_name='clusterware-hosts'
 
 if [ -f ./${package_name}.zip ]; then
   echo "Replacing existing ${package_name}.zip in this directory"
