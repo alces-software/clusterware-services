860b6a182bfc230bb59d1cf94db0e3e3094cff07
James Muscat <james.muscat@alces-software.com>
2018-03-08 17:57:25 +0000

    Perform post-installation initialisation of session repo.

--- clusterware-sessions/package/install.sh
+++ clusterware-sessions/package/install.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 cp -R data/* "${cw_ROOT}"
+
+mkdir -p "${cw_ROOT}"/opt/clusterware-sessions
+
+echo "Setting up session base repository"
+if [ -d "${cw_ROOT}/var/lib/sessions/repos" ]; then
+  echo 'Detected existing repository.'
+else
+  echo 'Initializing repository:'
+  if [ -f /tmp/clusterware-sessions.tar.gz ]; then
+    mkdir -p "${cw_ROOT}"/var/lib/sessions/repos/base
+    tar -C "${cw_ROOT}"/var/lib/sessions/repos/base -xzf /tmp/clusterware-sessions.tar.gz
+  else
+    export cw_SESSION_rev cw_SESSION_track
+    "${cw_ROOT}/bin/alces" session update
+    "${cw_ROOT}/bin/alces" session enable default
+  fi
+fi
