f27f83cfe8e01ed326edea1a8ed2c5bdded57b06
James Muscat <james.muscat@alces-software.com>
2018-03-22 15:06:44 +0000

    Package up the `configure-ephemeral-disks` profile for Forge.

---
+++ configure-ephemeral-disks/package/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -R data/* "${cw_ROOT}"
+
+if [ ! -d "${cw_ROOT}/etc/config/cluster" ]; then
+  echo "Cluster not yet configured. Deferring ephemeral disk configuration until next boot."
+else
+  "${cw_ROOT}/etc/handlers/configure-ephemeral-disks/configure"
+fi
