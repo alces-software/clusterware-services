c23f8ab3be027dbcdffdb423419d7a943bc76a29
James Muscat <james.muscat@alces-software.com>
2018-03-22 17:30:48 +0000

    Packages for OpenLava compute and master.

---
+++ openlava-master/package/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+package_name='openlava-master'
+
+if [ -f ./${package_name}.zip ]; then
+  echo "Replacing existing ${package_name}.zip in this directory"
+  rm ./${package_name}.zip
+fi
+
+temp_dir=$(mktemp -d /tmp/${package_name}-build-XXXXX)
+
+cp -r * "${temp_dir}"
+
+# Nothing else to do
+
+pushd "${temp_dir}" > /dev/null
+zip -r ${package_name}.zip *
+popd > /dev/null
+
+mv "${temp_dir}"/${package_name}.zip .
+
+rm -rf "${temp_dir}"
